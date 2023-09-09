a7a6705f0420d155f218caf4219128251dc53a56
Mirudan <chaos.73@yandex.ru>
2023-09-09 16:11:44 +0400

    ADD: Class MixinLang & class Keyboard

---
+++ src/keyboard.py
@@ -0,0 +1,27 @@
+from src.item import Item
+
+
+class MixinLang:
+    """Миксин-класс для изменения и хранения раскладки клавиатуры"""
+
+    def __init__(self, language="EN"):
+        """иницилизатор с языком по-умолчанию"""
+        self.__language = language
+
+    @property
+    def language(self):
+        """выводит раскладку клавиатуры"""
+        return self.__language
+
+    def change_lang(self):
+        """метод для изменения раскладки клавиатуры"""
+        if self.language == "EN":
+            self.__language = "RU"
+        elif self.language == "RU":
+            self.__language = "EN"
+        return self
+
+
+class Keyboard(Item, MixinLang):
+    """класс для представления клавиатуры в магазине, дочерний класс Item """
+    ...
